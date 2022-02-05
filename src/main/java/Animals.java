43d3a2e5a990799cba871158bf0633ccb2cbfaa3
Mohamed A. Ibrahim <rageali12@gmail.com>
2022-02-05 10:30:33 +0300

    added instance variable, constructor, getters and setters

---
+++ src/main/java/Animals.java
@@ -0,0 +1,25 @@
+public class Animals {
+    int id;
+    String name;
+
+    public Animals(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
