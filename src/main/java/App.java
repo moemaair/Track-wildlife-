997ccb2bdcd108158adcca0affd166bda2b8117a
Mohamed A. Ibrahim <rageali12@gmail.com>
2022-02-08 23:06:09 +0300

    added methods to my get method to capture all list from dao

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,24 +1,23 @@
 import dao.Sql2oAnimals;
 import modal.Animals;
-import modal.EndangeredAnimals;
 import org.sql2o.Sql2o;
-import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static spark.Spark.*;
+import static spark.Spark.* ;
 
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
 
 
-        String connectionString = "jdbc:postgresql://localhost:5432/wildlife_tracker"; //connect to database wildlife_tracker
-        Sql2o sql2o = new Sql2o(connectionString, "posgres", "bnmk");
-
+        String connectmetodatabase = "jdbc:postgresql://localhost:5432/wildlife_tracker"; //connect to database wildlife_tracker
+        Sql2o sql2o = new Sql2o(connectmetodatabase, "postgres", "bnmk");
+        Sql2oAnimals sql2oAnimals = new Sql2oAnimals(sql2o);
+//        Sql2oEanimals sql2oEanimals = new Sql2oEanimals(sql2o);
 
         // home route
         get("/", (req, res) ->{
@@ -29,6 +28,8 @@ public class App {
         //animal route
         get("/animals/new", (req, res) ->{
             Map<String,Object> model = new HashMap<>();
+            List<Animals> animals = sql2oAnimals.getAll();
+            model.put("animals", animals);
             return modelAndView(model, "animal.hbs");
         },new HandlebarsTemplateEngine());
 
@@ -37,27 +38,28 @@ public class App {
             int id = Integer.parseInt(req.queryParams("id"));
             String name = req.queryParams("name");
             Animals newAnimals = new Animals(id, name);
-            return new ModelAndView(model, "animalList.hbs");
+            sql2oAnimals.add(newAnimals);
+            res.redirect("/animals/new");
+            return null;
         }, new HandlebarsTemplateEngine());
 
         //retrieving all animals
-        get("/animals", (request, response) -> {
-            Map<String, Object> model = new HashMap<String, Object>();
-            return new ModelAndView(model, "animalList.hbs");
-        }, new HandlebarsTemplateEngine());
+//        get("/animals", (request, response) -> {
+//            Map<String, Object> model = new HashMap<String, Object>();
+//            return new ModelAndView(model, "animalList.hbs");
+//        }, new HandlebarsTemplateEngine());
 
 
         //endangered route
-        get("endangeredAnimal", (req,res)->{
-            Map<String, Object> model = new HashMap<String, Object>();
-
-            return new ModelAndView(model,"endangeredanimal.hbs");
-        }, new HandlebarsTemplateEngine());
-
-        post("/endangeredList", (request, response) -> {
-            Map<String, Object> model = new HashMap<String, Object>();
-            return new ModelAndView(model,"endangeredList.hbs");
-        }, new HandlebarsTemplateEngine());
+//        get("/endangeredanimal/new", (req,res)->{
+//            Map<String, Object> model = new HashMap<String, Object>();
+//            return new ModelAndView(model,"endangeredanimal.hbs");
+//        }, new HandlebarsTemplateEngine());
+//
+//        post("/endangeredList", (request, response) -> {
+//            Map<String, Object> model = new HashMap<String, Object>();
+//            return new ModelAndView(model,"endangeredList.hbs");
+//        }, new HandlebarsTemplateEngine());
 
 
     }
