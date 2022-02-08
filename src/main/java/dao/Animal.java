d94f84418a68e882e9bf12ffd754d288f732c61f
Mohamed A. Ibrahim <rageali12@gmail.com>
2022-02-08 23:04:14 +0300

    implemented looping and branching for this template

--- src/main/java/dao/Animal.java
+++ src/main/java/dao/Animal.java
@@ -1,8 +1,12 @@
+package dao;
+
 import modal.Animals;
+import modal.EndangeredAnimals;
 
 import java.util.List;
 
 public interface wildlifedao {
     List<Animals> getAll();
     void add(Animals animal);
+
 }
