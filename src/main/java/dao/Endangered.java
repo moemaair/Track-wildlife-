1656a9fa38a5c5f316f9869c27d3fba77fd8360f
Mohamed A. Ibrahim <rageali12@gmail.com>
2022-02-08 23:11:45 +0300

    Endangered Animal interface

--- src/main/java/dao/Endangered.java
+++ src/main/java/dao/Endangered.java
@@ -1,4 +1,10 @@
 package dao;
 
+import modal.EndangeredAnimals;
+
+import java.util.List;
+
 public interface Endangered {
+    List<EndangeredAnimals> getAll();
+    void add(EndangeredAnimals animal);
 }
