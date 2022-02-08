20217e7da6daac6410067e9b724cff7e8d400619
Mohamed A. Ibrahim <rageali12@gmail.com>
2022-02-08 23:25:36 +0300

    added post route of endangered animal

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,10 @@
 import dao.Sql2oAnimals;
+import dao.Sql2oEanimals;
 import modal.Animals;
+
+import modal.EndangeredAnimals;
 import org.sql2o.Sql2o;
+import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.util.HashMap;
@@ -17,7 +21,7 @@ public class App {
         String connectmetodatabase = "jdbc:postgresql://localhost:5432/wildlife_tracker"; //connect to database wildlife_tracker
         Sql2o sql2o = new Sql2o(connectmetodatabase, "postgres", "bnmk");
         Sql2oAnimals sql2oAnimals = new Sql2oAnimals(sql2o);
-//        Sql2oEanimals sql2oEanimals = new Sql2oEanimals(sql2o);
+        Sql2oEanimals sql2oEanimals = new Sql2oEanimals(sql2o);
 
         // home route
         get("/", (req, res) ->{
@@ -50,16 +54,25 @@ public class App {
 //        }, new HandlebarsTemplateEngine());
 
 
-        //endangered route
-//        get("/endangeredanimal/new", (req,res)->{
-//            Map<String, Object> model = new HashMap<String, Object>();
-//            return new ModelAndView(model,"endangeredanimal.hbs");
-//        }, new HandlebarsTemplateEngine());
-//
-//        post("/endangeredList", (request, response) -> {
-//            Map<String, Object> model = new HashMap<String, Object>();
-//            return new ModelAndView(model,"endangeredList.hbs");
-//        }, new HandlebarsTemplateEngine());
+//        endangered route
+        get("/endangeredanimal/new", (req,res)->{
+            Map<String, Object> model = new HashMap<String, Object>();
+            List<EndangeredAnimals> endangeredAnimals = sql2oEanimals.getAll();
+            return new ModelAndView(model,"endangeredanimal.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/endangeredanimals", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            int id = Integer.parseInt(request.queryParams("id"));
+            String name = request.queryParams("name");
+            String health = request.queryParams("health");
+            int age = Integer.parseInt(request.queryParams("age"));
+
+            EndangeredAnimals newEanimals = new EndangeredAnimals(id, name, health, age);
+            sql2oEanimals.add(newEanimals);
+            response.redirect("/endangeredanimal/new");
+            return null;
+        }, new HandlebarsTemplateEngine());
 
 
     }
